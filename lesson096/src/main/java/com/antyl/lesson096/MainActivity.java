77dbddc5c5ecd36da3b8d5952517abee5e8e19fe
Unknown <antyl93@gmail.com>
2017-11-07 09:32:27 +0700

    add Service to Lesson096

--- lesson096/src/main/java/com/antyl/lesson096/MainActivity.java
+++ lesson096/src/main/java/com/antyl/lesson096/MainActivity.java
@@ -1,13 +1,54 @@
 package com.antyl.lesson096;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    final int TASK1_CODE = 1;
+    final int TASK2_CODE = 2;
+    final int TASK3_CODE = 3;
+
+    public final static int STATUS_START = 100;
+    public final static int STATUS_FINISH = 200;
+
+    public final static String PARAM_TIME = "time";
+    public final static String PARAM_TASK = "task";
+    public final static String PARAM_RESULT = "result";
+    public final static String PARAM_STATUS = "status";
+
+    public final static String BROADCAST_ACTION = "ru.startandroid.develop.p0961servicebackbroadcast";
+
+
+    TextView tvTask1;
+    TextView tvTask2;
+    TextView tvTask3;
+    BroadcastReceiver br;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTask1 = (TextView) findViewById(R.id.tvTask1);
+        tvTask1.setText("Task1");
+        tvTask2 = (TextView) findViewById(R.id.tvTask2);
+        tvTask2.setText("Task2");
+        tvTask3 = (TextView) findViewById(R.id.tvTask3);
+        tvTask3.setText("Task3");
+
+        br = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int task = intent.getIntExtra(PARAM_TASK, 0);
+                int status = intent.getIntExtra(PARAM_STATUS, 0);
+            }
+        };
     }
 }
